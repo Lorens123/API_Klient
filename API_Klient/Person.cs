74bc6581f0130eb49d58872989558801b14f56c8
Lorens123 <lorens.vinge@gmail.com>
2021-12-06 13:05:20 +0100

    first commit

---
+++ API_Klient/Person.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class Person
+{
+    public string name { get; set; }
+    public string height { get; set; }
+    public string mass { get; set; }
+}
---
+++ API_Klient/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.AccessControl;
+using RestSharp;
+using System.Text.Json;
+
+
+
+
+
+RestClient pokeClient = new RestClient("https://pokeapi.co/api/v2/");
+RestRequest pokeRequest = new RestRequest("pokemon/pikachu");
+
+IRestResponse pokeResponse = pokeClient.Get(pokeRequest);
+
+// Console.WriteLine(pokeResponse.Content);
+
+
+
+// RestClient swClient = new RestClient("https://swapi.dev/api/");
+// RestRequest swRequest = new RestRequest("people/3");
+// IRestResponse swResponse = swClient.Get(swRequest);
+
+// // Console.WriteLine(swResponse.Content);
+
+
+Pokemon p = JsonSerializer.Deserialize<Pokemon>(pokeResponse.Content);
+
+Console.WriteLine(p.name);
+Console.WriteLine(p.weight);
+Console.WriteLine(p.is_default);
+
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
